4d834e2f624f3e7c3fe9ba03d506650e68838f55
Kevin-R-Ho <kevin.r..ho@outlook.com>
2021-12-11 21:56:15 -0800

    .js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from "./pages";
-import SigninPage from "./pages/signin";
+import Home from "./pages.js";
+import SigninPage from "./pages/signin.js";
 
 function App() {
   return (
